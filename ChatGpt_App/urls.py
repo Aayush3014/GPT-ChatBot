6407deae2296823b302f9fdebffb2d1c164a2f9f
Aayush3014 <ayushak3008@gmail.com>
2023-10-26 23:49:26 +0530

    Registration

--- ChatGpt_App/urls.py
+++ ChatGpt_App/urls.py
@@ -4,4 +4,7 @@ from ChatGpt_App import views
 
 urlpatterns = [
     path("", views.chatbot, name="chatbot"),
+    path("login", views.login, name="login"),
+    path("register", views.register, name="register"),
+    path("logout", views.logout, name="logout"),
 ]
--- ChatGpt_App/views.py
+++ ChatGpt_App/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import JsonResponse
 import openai
-
+from django.contrib import auth
+from django.contrib.auth.models import User
 # Create your views here.
 
 
@@ -35,3 +36,36 @@ def chatbot(request):
     return render(request, "chatbot.html")
 
     
+
+
+def login(request):
+    return render(request, "login.html")
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+
+
+        if password1 == password2:
+
+            try:
+                user = User.objects.create_user(username=username, email=email, password=password1)
+                user.save()
+                auth.login(request, user)
+                return redirect("chatbot")
+            except:
+                error_message = "Error Creating Account"
+                return render(request, "register.html", {"error": error_message})
+        else:
+            error_message = "Password does not match"
+            return render(request, "register.html", {"error": error_message})
+    
+    return render(request, "register.html")
+
+
+def logout(request):
+    auth.logout(request)
